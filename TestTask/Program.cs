3890882dd1f1e9da868d837aaeab09f741108c4f
Nikolay_Isupov <kedichboss@gmail.com>
2021-08-19 13:13:23 +0300

    Task completed

---
+++ TestTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            if (str.Length > 100)
+            {
+                throw new Exception("String is longer than 100 characters");
+            }
+            Console.WriteLine("Reversed string:");
+            Console.WriteLine(ReverseString(str));
+        }
+        public static string ReverseString(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            return ReverseString(str.Substring(1)) + str[0];
+        }
+    }
+}
